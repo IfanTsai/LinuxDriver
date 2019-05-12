354f74313f9df6a78a9f5ef2b00c4d8f0701bf75
IfanTsai <1558499691@qq.com>
2019-05-12 22:14:32 +0800

    add input/input_app.c

---
+++ input/input_app.c
@@ -0,0 +1,35 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <linux/input.h>
+
+#define KEYBOARD_PATH "/dev/input/event5"
+#define MOUSE_PATH "/dev/input/event7"
+
+int main(void)
+{
+    int fd = open(MOUSE_PATH, O_RDONLY);
+    if (fd < 0) {
+        perror("open");
+        exit(1);
+    }
+
+    struct input_event event = { 0 };
+    while (1) {
+        if (read(fd, &event, sizeof(struct input_event)) < 0) {
+            perror("read");
+            exit(1);
+        }
+        printf("\n----------------start---------------------\n");
+        printf("type  = %u\n", event.type);
+        printf("code  = %u\n", event.code);
+        printf("value = %u\n", event.value);
+        printf("----------------end---------------------\n");
+    }
+
+    close(fd);
+    return 0;
+}
